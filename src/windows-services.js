80b23511dc6ad33fab7385fa9665a505ebeff224
juurr00 <ihnsvLL853>
2022-01-09 21:40:14 +0100

    Added Windows "getPrinters()" function

--- src/windows-services.js
+++ src/windows-services.js
@@ -1,34 +1,60 @@
-async function getPrinters() {
-    function stdoutHandler(stdout) {
-        const printers = [];
+const {spawn} = require('child_process');
 
-        stdout
-            .split(/(\r?\n){2,}/)
-            .map((printer) => printer.trim())
-            .filter((printer) => !!printer)
-            .forEach((printer) => {
-                const {isValid, printerData} = isValidPrinter(printer);
+exports.getPrinters = () => {
+    return new Promise((resolve, reject) => {
 
-                if (!isValid) return;
-
-                printers.push(printerData);
-            });
-
-        return printers;
-    }
-
-    try {
-        const {stdout} = await execFileAsync("Powershell.exe", [
+        // Node’s Child Processes
+        // https://jscomplete.com/learn/node-beyond-basics/child-processes
+        const proces = spawn("Powershell.exe", [
             "-Command",
             "Get-CimInstance Win32_Printer -Property DeviceID,Name",
         ]);
-        return stdoutHandler(stdout);
-    } catch (error) {
-        throw error;
-    }
+
+        let stdout = '';
+        proces.on("exit", function (code, signal) {
+            if (code === 0) {
+                resolve(stdoutHandler(stdout));
+                return;
+            }
+            reject("child process exited with " + `code ${code} and signal ${signal}`);
+        });
+        proces.on("error", function (code, signal) {
+            reject("child process error with " + `code ${code} and signal ${signal}`);
+        });
+        proces.stdin.on("error", function (error) {
+            reject("stdin process error: " + error);
+        });
+
+        proces.stdout.on("data", data => {
+            console.info(`child stdout: ${data}`);
+            stdout += data;
+        });
+
+        proces.stderr.on("data", data => {
+            reject(`stderr error: \n${data}`);
+        });
+        proces.stdin.end();
+    });
 }
 
-exports.getPrinters = getPrinters()
+
+function stdoutHandler(stdout) {
+    const printers = [];
+
+    stdout
+        .split(/(\r?\n){2,}/)
+        .map((printer) => printer.trim())
+        .filter((printer) => !!printer)
+        .forEach((printer) => {
+            const {isValid, printerData} = isValidPrinter(printer);
+
+            if (!isValid) return;
+
+            printers.push(printerData);
+        });
+
+    return printers;
+}
 
 function isValidPrinter(printer) {
     const printerData = {
@@ -55,3 +81,11 @@ function isValidPrinter(printer) {
         printerData,
     };
 }
+
+exports.getPrinters()
+.then(result => {
+    console.log('==== success: ' + JSON.stringify(result, null, 4))
+})
+    .catch(error => {
+    console.log('==== error: ' + error)
+})
